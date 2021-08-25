87bc6acc541fc8cd3cc8fc846afa83efb1fc6783
eduard-martinez <edfemagonza@gmail.com>
2021-08-25 16:47:25 -0500

    add data

---
+++ README.md
@@ -0,0 +1,25 @@
+![GitHub](https://img.shields.io/github/license/taller-R/clase_1) [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/taller-R/clase_1/issues) ![](https://img.shields.io/github/followers/taller-R?style=social)
+
+<img src="https://avatars0.githubusercontent.com/u/69440432?s=400&u=96b3e58c713578b563d5c3d3c259f34965ac8e33&v=4" align="right" width=120 height=120 alt="" />
+
+# 1. Descargar este repositorio
+
+Puedes descargar esta clase de 3 diferentes formas:
+
+- 1.1. Descargar repositorio manualmente. [Ver como hacerlo aquí](https://raw.githubusercontent.com/taller-R/clase_1/master/help/pics/download.gif)
+
+- 1.2. Crear proyecto con versión de control de Git. [Ver como hacerlo aquí](https://raw.githubusercontent.com/taller-R/clase_1/master/help/pics/crear_proyecto.gif)
+
+- 1.3. Clonar el repositorio desde la terminal. [Ver como hacerlo aquí](https://github.com/taller-R/Clase_1/blob/master/help/pics/terminal.gif)
+
+Nota: Para hacerlo de las formas **1.2.** o **1.3.** debe verificar si ya tiene instalado [Git](https://git-scm.com/downloads) en su equipo. Puedes verificar la versión de [Git](https://git-scm.com/downloads) escribiendo la siguiente línea de código `git --version` sobre la terminal de tu equipo.
+
+# 2. Estructura de la clase
+
+La clase está orientada a 2 objetivos: 
+
+- 2.1. Introducir a los estudiantes sobre los conceptos de R a desarrollar en la clase. La presentación de la clase se encuentran en la carpeta **help** bajo el nombre **intro-to-class.html**.
+
+- 2.2. Aplicar los conceptos vistos en clase. Para cumplir con este objetivo, el estudiante deberá revisar el script de la clase que se encuentra en la carpeta **scr** y resolver algunos task que se encuentran a los largo de este.
+
+
---
+++ data_3/scr/scr.R
@@ -0,0 +1,147 @@
+# Elaborado por: Eduard Martinez
+# Colaboradores:
+# Fecha de elaboracion: 25/08/2021
+# Ultima modificacion: 25/08/2021
+
+# configuracion inicial
+rm(list = ls()) # limpia el entorno de R
+if(!require(pacman)) install.packages(pacman)
+require(pacman)
+p_load(dplyr,data.table)
+print('No fijamos un WD porque estamos trabajando en un R-project')
+
+#============================#
+# [1.] Directorio de trabajo #
+#============================#
+
+getwd() # obtener la ruta del directorio de trabajo actual
+
+list.files() # obtener vector nombres de archivos en el wd
+
+list.files(path = '.' , pattern = '.md') # vector nombres archivos markdown (.md)
+
+#====================#
+# [2.] Tipo de datos #
+#====================#
+
+# lógico
+vector_l = c(NA,TRUE,FALSE)
+is.logical(vector_l)
+
+# character
+vector_c = c("hola",'a') #para character se usa '' o ""
+is.character(vector_c)
+
+# Datos numericos
+
+# numeric
+vector_n = c(5,2)
+is.numeric(vector_n)
+
+# interger
+vector_i = -5:5
+is.integer(vector_i)
+
+# double
+vector_d = c(1e6,2e6)
+is.double(vector_d)
+
+print("puedo consultar el tipo de datos que contiene un objeto usando la función is()").
+
+#==========================#
+# [3.] Estructura de datos #
+#==========================#
+
+#--------------------------------------
+# Dim |  Homogeneos	 |  Heterogeneos  |
+#--------------------------------------
+#  1d	|    Vector    |   Listas       |
+#  2d	|    Matriz    |   Dataframe    |
+#  nd	|    Array     |                |
+#--------------------------------------
+
+# Homogéneos: todos los elementos de este objeto deben ser del mismo tipo (númerico o carácter o lógico).
+
+# Heterogéneos: este tipo de objetos permiten almacenar diferentes tipos de datos (númerico, carácter y lógico).
+
+# Dimensiones: hace referencia al numero de dimensiones (filas y/o columnas) de un objeto.
+
+
+## Vector & Matriz
+
+# Vectores
+abe = c("a","b","c","d","e","f")
+log = c(TRUE,FALSE,TRUE,TRUE,FALSE,FALSE)
+num = c(2,4,6,7,8,9) # los numeros no tiene que ser continuos
+
+# Matriz
+matriz = matrix(num, ncol = 3,  nrow = 2 )
+matriz
+
+## Listas 
+
+#lista              
+lista = list()
+lista[[1]] = abe
+lista[[2]] = log
+lista[[3]] = num
+lista[[4]] = matriz # matriz que creamos anteriormente
+lista
+
+## Dataframes
+
+#dataframe
+
+dataframe = data.frame(log, abe, num, matriz) 
+
+dataframe 
+
+## Manipular matrices
+
+matriz[1,]# todos los elementos en fila 1
+
+
+matriz[,2] # todos los elementos en columna 2
+
+
+matriz[1,2] # elemento en fila 1 y columna 2
+
+# Remplazar elementos en una fila
+matriz[1,] = c(3,7,9)
+matriz
+
+# remplazar un elemento
+matriz[1,2] = 5
+matriz
+
+# nombre de columnas
+colnames(matriz)  
+
+# nombre de fillas
+rownames(matriz) 
+
+# Asignar nombre a varias columnas
+colnames(matriz) = c("Col 1" , "Col 2", "col 3") 
+matriz
+
+# Asignar nombre a una fila en especifico
+rownames(matriz)[2] = "row 2" 
+matriz
+
+## Manipular dataframes
+
+dataframe[1,]# observar una fila
+
+# cambiar los nombre de la filla
+rownames(dataframe) = c("row 1","row 2","row 3","row 4","row 5","row 6") 
+
+rownames(dataframe)# observar los nombres de las fillas
+
+## Manipular listas
+
+lista[[4]] # LLamamos el dato que deseamos
+
+lista[[4]][,2] # seleccionar columna dentro de la matiz
+
+
+
